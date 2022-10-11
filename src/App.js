e2a36ed29e20f6aee2810624b0ebcc53e7cd6b14
Diego <drhiguerag@unal.edu.co>
2022-10-11 15:41:53 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { loginWithRedirect, user, isAuthenticated, logout, loginWithPopup } = useAuth0();
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {  isAuthenticated ? (
+          <>
+          <h1>Hola {user.name}</h1>
+          <div>
+            <img src={user.picture} alt={user.name} loading="lazy"/>
+          </div>
+          <button onClick={logout}>Logout</button>
+          </>
+        ) : (
+          <>
+            {/* <button onClick={() => loginWithPopup()}>LogIn Email</button> */}
+            <button onClick={() => loginWithPopup({ connection: "google-oauth2" })}>LogIn Gmail</button>
+          </>
+        )
+
+        }
     </div>
   );
 }
